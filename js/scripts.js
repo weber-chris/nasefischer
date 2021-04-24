45abba399bea26350d548c342b4c1fa0d38cdb77
Christoph Weber <christoph.t.weber@gmail.com>
2021-04-24 19:15:50 +0200

    updated gallery, linked reports to calendar

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-(function($) {
-    "use strict"; 
-    
-    	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
+(function ($) {
+    "use strict";
+
+    /* Preloader */
+    $(window).on('load', function () {
+        var preloaderFadeOutTime = 500;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function () {
+                preloader.fadeOut(preloaderFadeOutTime);
+            }, 500);
         }
         function load_google_sheet() {
             let spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1M-Ocs-a9Ri_9u9-DH_GQNgnnhQRD8v31L2KGMS2blro/edit?usp=sharing';
@@ -19,73 +19,93 @@
             })
         }
 
-        function create_table_cell(row_index, column_name, data){
+        function create_table_cell(row_index, column_name, data) {
             let td = document.createElement('td');
-            td.appendChild(document.createTextNode( data[row_index][column_name])) ;
-            return td 
+            td.appendChild(document.createTextNode(data[row_index][column_name]));
+            return td
         }
+
+        function create_report_cell(row_index, column_name, data) {
+            let report = data[row_index]['Bericht'];
+
+            let td = document.createElement('td');
+            let textNode = document.createTextNode(data[row_index][column_name]);
+            let link = document.createElement('a');
+            link.appendChild(textNode);
+            link.title = data[row_index][column_name];
+            link.href = report
+
+            td.appendChild(link);
+            return td
+        }
+
         function read_sheets(data) {
             let members = data.members.elements;
             let calendar = data.calendar.elements;
 
             let member_table = document.getElementById('member-table');
-            for (let row_index in members){
+            for (let row_index in members) {
                 let tr = document.createElement('tr');
-                tr.appendChild( create_table_cell(row_index, 'Name', members));
-                tr.appendChild( create_table_cell(row_index, 'Fangzahl', members));
-                tr.appendChild( create_table_cell(row_index, 'Schätzung', members));
-                tr.appendChild( create_table_cell(row_index, 'Strikes', members));
+                tr.appendChild(create_table_cell(row_index, 'Name', members));
+                tr.appendChild(create_table_cell(row_index, 'Fangzahl', members));
+                tr.appendChild(create_table_cell(row_index, 'Schätzung', members));
+                tr.appendChild(create_table_cell(row_index, 'Strikes', members));
                 member_table.appendChild(tr);
             }
 
             let calendar_table = document.getElementById('calendar-table');
-            for (let row_index in calendar){
+            for (let row_index in calendar) {
                 let tr = document.createElement('tr');
-                tr.appendChild( create_table_cell(row_index, 'Datum', calendar));
-                tr.appendChild( create_table_cell(row_index, 'Wo', calendar));
-                tr.appendChild( create_table_cell(row_index, 'Was', calendar));
+                tr.appendChild(create_table_cell(row_index, 'Datum', calendar));
+                tr.appendChild(create_table_cell(row_index, 'Wo', calendar));
+                if (calendar[row_index]['Bericht'] !== '') {
+                    tr.appendChild(create_report_cell(row_index, 'Was', calendar));
+                } else {
+                    tr.appendChild(create_table_cell(row_index, 'Was', calendar));
+                }
+
                 calendar_table.appendChild(tr);
             }
-            
+
             // filter_table(false);
 
         }
 
         hidePreloader();
-        
-        load_google_sheet();
-	});
 
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+        load_google_sheet();
     });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function () {
+        if ($(".navbar").offset().top > 20) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
     // closes the responsive menu on menu item click
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
+    $(".navbar-nav li a").on("click", function (event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
     });
-    
+
     // CALENDAR
-    function filterCalendar(calendar_table, show_future){
-        for (let row_index= 0; row_index < calendar_table.children.length; row_index++){
+    function filterCalendar(calendar_table, show_future) {
+        for (let row_index = 0; row_index < calendar_table.children.length; row_index++) {
             calendar_table.children[row_index].style = "";
         }
 
@@ -101,27 +121,25 @@
         //   }
     }
 
-    function filter_table(show_future){
+    function filter_table(show_future) {
         // currently disabled
-        $('#calendar-table tr').each(function() {
+        $('#calendar-table tr').each(function () {
             // https://stackoverflow.com/questions/7151543/convert-dd-mm-yyyy-string-to-date
-            let date_object = new Date($(this.children[0]).text().replace( /(\d{2}).(\d{2}).(\d{4})/, "$2/$1/$3"))
-            if (show_future){
-                if (date_object < new Date())
-                {
+            let date_object = new Date($(this.children[0]).text().replace(/(\d{2}).(\d{2}).(\d{4})/, "$2/$1/$3"))
+            if (show_future) {
+                if (date_object < new Date()) {
                     $(this).hide();
                 }
-            }else{
-                if (date_object > new Date() || isNaN(date_object))
-                {
+            } else {
+                if (date_object > new Date() || isNaN(date_object)) {
                     $(this).hide();
                 }
             }
         });
     }
-    $('#calendar_toggle').on('change', function() {
+    $('#calendar_toggle').on('change', function () {
         let calendar_table = document.getElementById('calendar-table')
-        
+
         $('tr').show();
         filter_table(this.checked);
     });
@@ -130,26 +148,26 @@
     var slideIndex = 1;
     showSlides(slideIndex);
 
-    $('.prev').on('click', function() {
+    $('.prev').on('click', function () {
         showSlides(slideIndex -= 1);
     });
-    $('.next').on('click', function() {
+    $('.next').on('click', function () {
         showSlides(slideIndex += 1);
     });
 
     function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("mySlides");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
 
-        slides[slideIndex-1].style.display = "flex";
+        slides[slideIndex - 1].style.display = "flex";
     }
-    
-    $('#btn_statutes').on('click',function(){
+
+    $('#btn_statutes').on('click', function () {
         expandStatutes()
     })
 
@@ -157,36 +175,36 @@
         var dots = document.getElementById("expand-dots");
         var moreText = document.getElementById("expand-more");
         var btnText = document.getElementById("btn_statutes");
-      
+
         if (dots.style.display === "none") {
-          dots.style.display = "inline";
-          btnText.innerHTML = "Aufklappen";
-          moreText.style.display = "none";
+            dots.style.display = "inline";
+            btnText.innerHTML = "Aufklappen";
+            moreText.style.display = "none";
         } else {
-          dots.style.display = "none";
-          btnText.innerHTML = "Zuklappen";
-          moreText.style.display = "inline";
+            dots.style.display = "none";
+            btnText.innerHTML = "Zuklappen";
+            moreText.style.display = "inline";
         }
-      }
+    }
 
 
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
+    $("input, textarea").keyup(function () {
+        if ($(this).val() != '') {
+            $(this).addClass('notEmpty');
+        } else {
+            $(this).removeClass('notEmpty');
+        }
     });
 
 
-   
+
     /* Back To Top Button */
     // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > amountScrolled) {
             $('a.back-to-top').fadeIn('500');
         } else {
@@ -195,9 +213,9 @@
     });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+    /* Removes Long Focus On Buttons */
+    $(".button, a, button").mouseup(function () {
+        $(this).blur();
+    });
 
 })(jQuery);
